e63926586e8965a059cc3ce66dc00bc5dd4f7d95
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-10 18:47:34 +0100

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["https://ai-lesson-plan.vercel.app/", "localhost:3000"]
+          }
+    },
 };
 
 module.exports = nextConfig;
