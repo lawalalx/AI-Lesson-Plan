b0ab6a533812b4f320457001d85a6f89d75cc8bd
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-10 17:19:15 +0100

    etst

--- actions/getLessonPlan.tsx
+++ actions/getLessonPlan.tsx
@@ -4,12 +4,12 @@ import axios from 'axios';
 import {prisma} from '@/db/client';
 
 export const getLessonPlan = async (formData: FormData) => {
-  
-  const gradeLevel = formData.get('gradeLevel');
-  const topic = formData.get('topic');
-  const criteria = formData.get('criteria');
-  const standards = formData.get('standards');
-  const language = formData.get('language');
+
+  const gradeLevel = formData.get('gradeLevel') as string;
+  const topic = formData.get('topic') as string;
+  const criteria = formData.get('criteria') as string;
+  const standards = formData.get('standards') as string;
+  const language = formData.get('language') as string;
 
   if (!formData) {
     throw new Error('Form data is required.');
