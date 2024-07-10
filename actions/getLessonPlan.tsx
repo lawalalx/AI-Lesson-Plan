04fc80234d337affacb3db8566c9df34b77f20ab
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-10 17:41:07 +0100

    etst

--- actions/getLessonPlan.tsx
+++ actions/getLessonPlan.tsx
@@ -15,7 +15,6 @@ export const getLessonPlan = async (formData: FormData) => {
     throw new Error('Form data is required.');
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   const lessonNote = await prisma.alxuser.create({
     data: {
       gradeLevel,
